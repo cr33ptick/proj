21f9f2562a250d973d81986e3c6dd2b356f0a781
Joe-Mwangi <joem99271@gmail.com>
2024-04-03 14:03:52 +0300

    schema

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -15,47 +15,39 @@ const Navbar = async () => {
     <nav className="fixed h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
       <div className="flex h-14 items-center pl-4 justify-between border-b border-zinc-200">
         <Link href="/" className="flex text-primary text-xl z-40 font-semibold">
-          <span>Shareride Kenya</span>
+          <span>Freelance Doctors</span>
         </Link>
 
-        <div className="hidden items-center gap-4 pr-4 lg:flex">
-          <Link href="/publish/1" className="flex gap-4 text-md font-bold">
-            <CirclePlus />
-            Publish
-          </Link>
-          <div className=" items-center gap-4 pr-4 sm:flex">
-            {!user ? (
-              <>
-                <LoginLink
-                  className={buttonVariants({
-                    variant: "ghost",
-                    size: "sm",
-                  })}
-                >
-                  Sign in
-                </LoginLink>
-                <RegisterLink
-                  className={buttonVariants({
-                    size: "sm",
-                  })}
-                >
-                  Register <ArrowRight className="ml-1.5 h-5 w-5" />
-                </RegisterLink>
-              </>
-            ) : (
-              <>
-                <UserAccountNav
-                  name={
-                    !user.given_name || !user.family_name
-                      ? "Your Account"
-                      : `${user.given_name} ${user.family_name}`
-                  }
-                  email={user.email ?? ""}
-                  imageUrl={user.picture ?? ""}
-                />
-              </>
-            )}
-          </div>
+        <div className="flex items-center gap-4 pr-4">
+          {!user ? (
+            <>
+              <Link
+                href="/login"
+                className={buttonVariants({
+                  variant: "outline",
+                  size: "sm",
+                })}
+              >
+                Login
+              </Link>
+              <Link
+                href="/register"
+                className={buttonVariants({
+                  size: "sm",
+                })}
+              >
+                Register
+              </Link>
+            </>
+          ) : (
+            <>
+              <UserAccountNav
+                name={user.username}
+                email={user.email ?? ""}
+                imageUrl={user.picture ?? ""}
+              />
+            </>
+          )}
         </div>
       </div>
     </nav>
