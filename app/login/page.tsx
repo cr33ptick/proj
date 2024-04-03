7133a45a13304f23c9dd2bbac35a996d40e8f6be
Joe-Mwangi <joem99271@gmail.com>
2024-04-03 18:56:39 +0300

    links

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,6 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import { Form } from "./form";
+import Link from "next/link";
 
 const Register = () => {
   return (
@@ -7,6 +8,14 @@ const Register = () => {
       <div className="flex flex-col gap-8 justify-center items-center py-8">
         <h2 className="font-bold text-3xl text-center lg:text-6xl">Login</h2>
         <Form />
+        <div className="flex items-center justify-center mt-6 cursor-pointer">
+          <Link
+            href="/register"
+            className="inline-flex items-center text-xs font-medium text-center text-primary hover:text-primary/75"
+          >
+            Don&#x27;t have an account?
+          </Link>
+        </div>
       </div>
     </MaxWidthWrapper>
   );
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,5 +1,6 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import { Form } from "./form";
+import Link from "next/link";
 
 const Register = () => {
   return (
@@ -7,6 +8,14 @@ const Register = () => {
       <div className="flex flex-col gap-8 justify-center items-center py-8">
         <h2 className="font-bold text-3xl text-center lg:text-6xl">Register</h2>
         <Form />
+        <div className="flex items-center justify-center mt-6 cursor-pointer">
+          <Link
+            href="/login"
+            className="inline-flex items-center text-xs font-medium text-center text-primary hover:text-primary/75"
+          >
+            Already have an account?
+          </Link>
+        </div>
       </div>
     </MaxWidthWrapper>
   );
